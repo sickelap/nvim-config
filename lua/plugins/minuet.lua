a4875a85ea3a0f40f5e379e0fd7c93c35ba479ce
sickelap <sickelap@pm.me>
2026-02-21 16:52:56 +0000

    update minuet keys

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -6,17 +6,11 @@ return {
         auto_trigger_ft = { "*" },
         keymap = {
           -- accept whole completion
-          accept = "<A-A>",
+          accept = "<C-S-y>",
           -- accept one line
-          accept_line = "<A-a>",
-          -- accept n lines (prompts for number)
-          -- e.g. "A-z 2 CR" will accept 2 lines
-          accept_n_lines = "<A-z>",
+          accept_line = "<C-y>",
           -- Cycle to prev completion item, or manually invoke completion
-          prev = "<A-[>",
-          -- Cycle to next completion item, or manually invoke completion
-          next = "<A-]>",
-          dismiss = "<A-e>",
+          dismiss = "<C-c>",
         },
       },
       provider = "openai_fim_compatible",
