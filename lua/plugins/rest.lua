8e8305d3a9a62d6e928c25fcb01268656a8769d4
sickelap <sickelap@pm.me>
2026-02-24 13:28:36 +0000

    add http lsp for completions and clenup

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,9 +1,33 @@
 return {
   {
     "lima1909/resty.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    setup = function()
-      vim.keymap.set({ "n", "v" }, "<leader>Rr", ":Resty run<CR>", { desc = "[R]esty [R]un request under the cursor" })
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      if vim.fn.executable "kulala-ls" == 0 then
+        vim.notify("Installing kulala-ls...", vim.log.levels.INFO)
+        vim.system({ "npm", "install", "-g", "@mistweaverco/kulala-ls" }, { text = true }, function(obj)
+          if obj.code == 0 then
+            vim.notify "kulala-ls installed. Restart nvim."
+          else
+            vim.notify("kulala-ls installation failed:\n" .. obj.stderr, vim.log.levels.ERROR)
+          end
+        end)
+      end
+
+      local lspconfig = require "lspconfig"
+      if not lspconfig.configs.kulala_ls then
+        lspconfig.configs.kulala_ls = {
+          default_config = {
+            cmd = { "kulala-ls" },
+            filetypes = { "http" },
+          },
+        }
+      end
+
+      lspconfig.kulala_ls.setup {}
     end,
   },
 
@@ -25,7 +49,6 @@ return {
           map("<leader>rr", "<cmd>Resty run<cr>", "Run HTTP request")
           map("<leader>rl", "<cmd>Resty last<cr>", "Re-run last request")
           map("<leader>rf", "<cmd>Resty favorite<cr>", "Favorite")
-          -- etc
         end,
       })
 
