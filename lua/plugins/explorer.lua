13da5e49ed795ff2eff44eb485a534856d69a2b0
sickelap <sickelap@pm.me>
2026-02-24 14:24:39 +0000

    move explorer related settings into one file

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -4,7 +4,6 @@ return {
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      -- Oil will take over directory buffers (e.g. `nvim .` or `:e src/`)
       default_file_explorer = true,
     },
   },
@@ -15,10 +14,46 @@ return {
       opts = opts or {}
       opts.filesystem = opts.filesystem or {}
 
-      -- IMPORTANT: don’t hijack opening directories; let oil handle `nvim .`
+      -- IMPORTANT: don’t hijack opening directories; let oil handle it
       opts.filesystem.hijack_netrw_behavior = "disabled"
 
       return opts
     end,
+  },
+
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      autocmds = {
+        neotree_grep = {
+          {
+            event = "FileType",
+            pattern = "neo-tree",
+            desc = "Add <leader>fw to grep inside selected folder in Neo-tree",
+            callback = function()
+              vim.keymap.set("n", "<leader>fw", function()
+                local manager = require "neo-tree.sources.manager"
+                local state = manager.get_state "filesystem"
+                local node = state.tree:get_node()
+                if not node then
+                  vim.notify("No node selected in Neo-tree", vim.log.levels.WARN)
+                  return
+                end
+
+                local path = node:get_id()
+                local is_dir = node.type == "directory" or vim.fn.isdirectory(path) == 1
+                local dir = is_dir and path or vim.fn.fnamemodify(path, ":h")
+
+                require("telescope.builtin").live_grep {
+                  search_dirs = { dir },
+                  prompt_title = "Grep in " .. dir,
+                }
+              end, { buffer = true, desc = "Live grep in selected Neo-tree folder" })
+            end,
+          },
+        },
+      },
+    },
   },
 }
