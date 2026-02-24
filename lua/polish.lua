dde6dd16b1a59934ec72adb2f8a0c0730d89966e
sickelap <sickelap@pm.me>
2026-02-24 14:51:24 +0000

    create diagnostics shortcut

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,3 +1,11 @@
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+vim.diagnostic.config { virtual_text = false, underline = false }
+
+vim.keymap.set(
+  "n",
+  "<leader>D",
+  function() vim.diagnostic.open_float(nil, { focus = false }) end,
+  { desc = "Show Diagnostocs" }
+)
